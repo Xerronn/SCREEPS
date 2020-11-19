b18849da1cf0424ead708971d9829f96fd4ee691
Xerronn <zonisick@gmail.com>
2020-11-19 11:41:38 -0500

    added automatic selling when over a threshold

--- system.logistics.js
+++ system.logistics.js
@@ -7,6 +7,11 @@ var systemLogistics = {
         for (var room of MY_ROOMS_TERMINAL) {
             var roomObject = Game.rooms[room];
             var roomTerminal = roomObject.terminal;
+            var roomOrders = _.filter(Game.market.orders, order => order.roomName == room);
+
+            // for (order of Object.keys(myOrders)) {
+            //     Game.market.cancelOrder(order);
+            // }
 
             //init memory
             if (!Memory.roomsPersistent[room].logistics) {
@@ -14,11 +19,21 @@ var systemLogistics = {
                 Memory.roomsPersistent[room].logistics.primary = roomObject.find(FIND_MINERALS)[0].mineralType;
                 Memory.roomsPersistent[room].logistics.needs = {};
                 Memory.roomsPersistent[room].logistics.haves = {};
+                Memory.roomsPersistent[room].logistics.selling = {};
+            }
+
+            //loop through sell orders
+            for (order of roomOrders) {
+                Memory.roomsPersistent[room].logistics.selling[order.resourceType] = order.remainingAmount;
             }
             
             //loop through contents and append resources that are in excess
             for (var mineral of MINERALS) {
-                amountStored = roomTerminal.store[mineral];
+                //subtract the amount we are selling from the amount we have
+                if (!Object.keys(Memory.roomsPersistent[room].logistics.selling).includes(mineral)) {
+                    Memory.roomsPersistent[room].logistics.selling[mineral] = 0;
+                }
+                amountStored = roomTerminal.store[mineral] - Memory.roomsPersistent[room].logistics.selling[mineral];
                 if (amountStored > BUFFER) {
                     //store in memory how much extra we have in the terminal
                     Memory.roomsPersistent[room].logistics.haves[mineral] = amountStored - BUFFER;
@@ -28,6 +43,10 @@ var systemLogistics = {
                     Memory.roomsPersistent[room].logistics.needs[mineral] = BUFFER - amountStored;
                     Memory.roomsPersistent[room].logistics.haves[mineral] = 0;
                 }
+            }
+
+            if (roomTerminal.cooldown > 0) {
+                continue; //move to next terminal
             }
 
             //check if other terminals need resources this one has
@@ -47,21 +66,24 @@ var systemLogistics = {
 
             //loop through haves and if there is a lot of extras, sell them
             for (var extra of Object.keys(Memory.roomsPersistent[room].logistics.haves)) {
-                if (roomTerminal.store[extra] > BUFFER * 2) {
+                if (roomTerminal.store[extra] -  Memory.roomsPersistent[room].logistics.selling[extra] > BUFFER * 2) {
                     let mineralInfo = Game.market.getHistory(extra);
                     let mineralInfoToday = mineralInfo[mineralInfo.length - 1];
                     let amountToSell = roomTerminal.store[extra] - (BUFFER * 2);
                     let price = (mineralInfoToday["avgPrice"] * 0.9).toFixed(3);
-                    let activeOrders = Game.market.orders;
 
-                    // let success = Game.market.createOrder({
-                    //     type: ORDER_SELL,
-                    //     resourceType: extra,
-                    //     price: price,
-                    //     totalAmount: amountToSell,
-                    //     roomName: room   
-                    // });
-                    console.log(JSON.stringify(activeOrders));
+                    let success = Game.market.createOrder({
+                        type: ORDER_SELL,
+                        resourceType: extra,
+                        price: price,
+                        totalAmount: amountToSell,
+                        roomName: room   
+                    });
+
+                    //if the deal is successfully placed, add it to the selling memory
+                    if (success == 0) {
+                        Memory.roomsPersistent[room].logistics.selling[extra] = amountToSell;
+                    }
                     break;
                 }
             }
